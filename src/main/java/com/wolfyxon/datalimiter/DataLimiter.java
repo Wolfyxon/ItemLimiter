4aa0a38750ab3591efa7245b9c3fc107fe125d82
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 00:04:22 +0200

    Initial event listeners

--- src/main/java/com/wolfyxon/datalimiter/DataLimiter.java
+++ src/main/java/com/wolfyxon/datalimiter/DataLimiter.java
@@ -1,17 +1,21 @@
 package com.wolfyxon.datalimiter;
 
+import com.wolfyxon.datalimiter.listeners.PlayerEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DataLimiter extends JavaPlugin {
 
+    public PlayerEvents playerEvents = new PlayerEvents(this);
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(playerEvents, this);
 
+        getLogger().info("Running");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Shutting down");
     }
 }
---
+++ src/main/java/com/wolfyxon/datalimiter/listeners/PlayerEvents.java
@@ -0,0 +1,34 @@
+package com.wolfyxon.datalimiter.listeners;
+
+import com.wolfyxon.datalimiter.DataLimiter;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+public class PlayerEvents  implements Listener {
+    DataLimiter plugin;
+
+    public PlayerEvents(DataLimiter plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player plr = e.getPlayer();
+    }
+
+    @EventHandler
+    public void nPlayerPickupItem(EntityPickupItemEvent e){
+        Entity entity = e.getEntity();
+        if(!(entity instanceof Player)) return;
+        Player plr = (Player) entity;
+        Item item = e.getItem();
+    }
+
+
+}
