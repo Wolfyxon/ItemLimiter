6ff35842f2448f097d70d2d5164640a24a51e3e4
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 10:28:44 +0200

    replaceMultiple()

--- src/main/java/com/wolfyxon/itemlimiter/Utils.java
+++ src/main/java/com/wolfyxon/itemlimiter/Utils.java
@@ -2,9 +2,17 @@ package com.wolfyxon.itemlimiter;
 
 import org.bukkit.ChatColor;
 
+import java.util.Map;
+
 public class Utils {
     public static String format(String text){
         if(text == null) return "";
         return ChatColor.translateAlternateColorCodes('&',text);
+    }
+    public static String replaceMultiple(String str, Map<String,String> replacements){
+        for(Map.Entry<String,String> e : replacements.entrySet()){
+            str.replace(e.getKey(),e.getValue());
+        }
+        return str;
     }
 }
