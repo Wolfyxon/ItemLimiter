7080c7382c38d612c0bad2d66a6853c9a0a2c6b3
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 15:35:06 +0200

    Added DataSizeCommand

---
+++ src/main/java/com/wolfyxon/itemlimiter/commands/DataSizeCommand.java
@@ -0,0 +1,35 @@
+package com.wolfyxon.itemlimiter.commands;
+
+import com.wolfyxon.itemlimiter.ItemLimiter;
+import com.wolfyxon.itemlimiter.ItemLimiterCommand;
+import com.wolfyxon.itemlimiter.ItemMgr;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class DataSizeCommand extends ItemLimiterCommand {
+
+    public DataSizeCommand(ItemLimiter plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if( !(sender instanceof Player)){
+            sendMsg("This command can only be run by a player.",sender);
+            return true;
+        }
+        Player plr = (Player) sender;
+        ItemStack item = plr.getInventory().getItemInMainHand();
+
+        if(item == null){
+            sendMsg("You're not holding any item.",sender);
+            return true;
+        }
+
+        sendMsg("Size of "+item.getType().toString()+": "+String.valueOf(ItemMgr.getSize(item)),sender);
+
+        return true;
+    }
+}
