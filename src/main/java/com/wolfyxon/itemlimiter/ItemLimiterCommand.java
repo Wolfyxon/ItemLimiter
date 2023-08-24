f648428d01182ad0804e845b23f48c85d1747ce8
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 15:17:17 +0200

    Implementations

--- src/main/java/com/wolfyxon/itemlimiter/ItemLimiterCommand.java
+++ src/main/java/com/wolfyxon/itemlimiter/ItemLimiterCommand.java
@@ -1,8 +1,13 @@
 package com.wolfyxon.itemlimiter;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
-public class ItemLimiterCommand {
+import java.util.List;
+
+public class ItemLimiterCommand implements CommandExecutor, TabCompleter {
     public ItemLimiter plugin;
 
     public ItemLimiterCommand(ItemLimiter plugin){
@@ -15,5 +20,15 @@ public class ItemLimiterCommand {
 
     public void sendMsg(String message, CommandSender sender){
         sender.sendMessage(Utils.format(message));
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
     }
 }
