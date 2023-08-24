d2029c80ed5965e862abad23cfc2406d2284b214
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 09:20:23 +0200

    Removed unused import

--- src/main/java/com/wolfyxon/datalimiter/listeners/PlayerEvents.java
+++ src/main/java/com/wolfyxon/datalimiter/listeners/PlayerEvents.java
@@ -8,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
 
 public class PlayerEvents  implements Listener {
     DataLimiter plugin;
