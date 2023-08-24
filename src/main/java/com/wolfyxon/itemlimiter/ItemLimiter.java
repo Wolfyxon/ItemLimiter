750bd0656cd0b62fa297286e06703f3c2849198b
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 15:30:08 +0200

    Command registral

--- src/main/java/com/wolfyxon/itemlimiter/ItemLimiter.java
+++ src/main/java/com/wolfyxon/itemlimiter/ItemLimiter.java
@@ -1,6 +1,7 @@
 package com.wolfyxon.itemlimiter;
 
 import com.wolfyxon.itemlimiter.listeners.PlayerEvents;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ItemLimiter extends JavaPlugin {
@@ -15,11 +16,19 @@ public final class ItemLimiter extends JavaPlugin {
         configMgr.load();
         getServer().getPluginManager().registerEvents(playerEvents, this);
 
+        registerCommand("itemlimiter", new ItemLimiterCommand(this));
+
         getLogger().info("Running");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Shutting down");
+    }
+
+    public void registerCommand(String name, ItemLimiterCommand handler){
+        PluginCommand command = getCommand(name);
+        command.setExecutor(handler);
+        if(Utils.hasMethod(handler,"onTabComplete")) command.setTabCompleter(handler);
     }
 }
