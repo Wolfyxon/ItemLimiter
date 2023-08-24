f72e8d25af464fc7e610691983dd246dd04239cb
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 10:13:27 +0200

    Initial book edit event

--- src/main/java/com/wolfyxon/itemlimiter/listeners/PlayerEvents.java
+++ src/main/java/com/wolfyxon/itemlimiter/listeners/PlayerEvents.java
@@ -7,7 +7,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerEditBookEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.meta.BookMeta;
 
 public class PlayerEvents  implements Listener {
     ItemLimiter plugin;
@@ -27,6 +29,13 @@ public class PlayerEvents  implements Listener {
         if(!(entity instanceof Player)) return;
         Player plr = (Player) entity;
         Item item = e.getItem();
+    }
+
+    @EventHandler
+    public void onPlayerEditBook(PlayerEditBookEvent e){
+        Player plr = e.getPlayer();
+        BookMeta newMeta = e.getNewBookMeta();
+
     }
 
 
