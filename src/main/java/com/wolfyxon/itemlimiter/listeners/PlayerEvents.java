e58da4bf33e27e5c56795ec5d208bfe90c885773
Wolfyxon <wolfyxon@gmail.com>
2023-08-24 15:09:42 +0200

    Inventory click detection

--- src/main/java/com/wolfyxon/itemlimiter/listeners/PlayerEvents.java
+++ src/main/java/com/wolfyxon/itemlimiter/listeners/PlayerEvents.java
@@ -9,6 +9,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCreativeEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
@@ -48,6 +50,15 @@ public class PlayerEvents  implements Listener {
         if (plugin.itemMgr.processItem(item,plr)){
             e.setCancelled(true);
             itemEntity.remove();
+            plr.sendMessage(getConfig().getMessage("itemRemoved").replace("{itemName}",item.getType().toString()));
+        }
+    }
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e){
+        ItemStack item = e.getCurrentItem();
+        Player plr = (Player) e.getWhoClicked();
+        if (plugin.itemMgr.processItem(item,plr)){
+            e.setCancelled(true);
             plr.sendMessage(getConfig().getMessage("itemRemoved").replace("{itemName}",item.getType().toString()));
         }
     }
