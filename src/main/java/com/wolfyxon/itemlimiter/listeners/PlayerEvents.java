f54e1aa18f5ab1fac1ae01f5ba77a62238f7dc2b
Wolfyxon <wolfyxon@gmail.com>
2023-08-30 17:10:28 +0200

    Book scanning after editing

--- src/main/java/com/wolfyxon/itemlimiter/listeners/PlayerEvents.java
+++ src/main/java/com/wolfyxon/itemlimiter/listeners/PlayerEvents.java
@@ -124,6 +124,13 @@ public class PlayerEvents  implements Listener {
             );
             e.setCancelled(true);
         }
+        if(getConfig().getItemScanningFeatureEnabled("scanOnBookEdit")){
+            item = ItemMgr.getItemInSlot(plr,e.getSlot()); //re-get item in case it was trimmed
+            if(getItemMgr().itemExceedsLimit(item)){
+                ItemMgr.removeItem(plr.getInventory(),e.getSlot());
+                sendItemRemoved(plr,item);
+            }
+        }
     }
 
 
